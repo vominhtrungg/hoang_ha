48e854bfaa3aeb4501c06e2d7d626367b7180dcf
vominhtrungg <vominhtrungg@gmail.com>
2024-06-04 22:52:01 +0700

    trung-nhanbaogia
    nhanbaogia

--- public/hoangha/detail/js/index.js
+++ public/hoangha/detail/js/index.js
@@ -1,28 +1,34 @@
 jQuery(document).ready(function($) {
-	var slideshow =$('#detail_slideshow');
-	slideshow.owlCarousel({
-	    items : 1,
-	    margin: 0,
-	    slideSpeed : 4000,
-		autoplaySpeed: 1000,
-		lazyLoad:true,
-	    nav: false,
-	    autoplay: true,
-	    dots: true,
-	    loop: true,
-	    
-	    animateOut: 'lightSpeedOut',
-    	animateIn: 'lightSpeedln',
+    var slideshow = $('#detail_slideshow');
+    slideshow.owlCarousel({
+        items: 1,
+        margin: 0,
+        slideSpeed: 4000,
+        autoplaySpeed: 1000,
+        lazyLoad: true,
+        nav: false,
+        autoplay: true,
+        dots: true,
+        loop: true,
 
-	 });
-	 $('#detail_slideshow').on('mouseover',function(e){
-	    slideshow.trigger('stop.owl.autoplay');
-	  });
-	  $('#detail_slideshow').on('mouseleave',function(e){
-	      slideshow.trigger('play.owl.autoplay');
-	  });
+        animateOut: 'lightSpeedOut',
+        animateIn: 'lightSpeedln',
 
-	  $(window).load(function(){
-	  		$("body,html").animate({scrollTop:$(".content").offset().top},1000);
-	  });
+    });
+    $('#detail_slideshow').on('mouseover', function(e) {
+        slideshow.trigger('stop.owl.autoplay');
+    });
+    $('#detail_slideshow').on('mouseleave', function(e) {
+        slideshow.trigger('play.owl.autoplay');
+    });
+
+    $(window).load(function() {
+        $("body,html").animate({ scrollTop: $(".content").offset().top }, 1000);
+    });
+    $(".contact-btn").on('click', function() {
+        $(".custom-model-main").addClass('model-open');
+    });
+    $(".close-btn, .bg-overlay").click(function() {
+        $(".custom-model-main").removeClass('model-open');
+    });
 });
